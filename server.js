7146273590273269fae0199d3c07125862f8543f
Mr Hlaing <tunw2359@gmail.com>
2026-09-09 12:49:25 +0700

    Revert specialty to specialization to fix 500 server error

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ const resourceDefinitions = {
   },
   teachers: {
     table: 'teachers',
-    fields: ['name', 'phone', 'email', 'specialty'],
+    fields: ['name', 'phone', 'email', 'specialization'],
     required: ['name'],
   },
   staff: {
